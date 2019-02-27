aeea6957f1feb9ac7e884d904bba3831a02ecc73
Victoria Kempter <victoria@thekempters.de>
2019-02-27 12:02:37 +0100

    fixed type in address

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :restaurant_id, presence: true
+
+end
